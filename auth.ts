5c7ceed94922f5e05b620b7611ff5df6f31a1cef
ricardocisco <ricardopenga70@gmail.com>
2024-11-27 02:22:42 -0300

    update auth

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   session: {
     strategy: "jwt",
   },
