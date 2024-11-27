29ed87c676c167f74ac513ae762bcf4c048b3aff
ricardocisco <ricardopenga70@gmail.com>
2024-11-27 01:03:05 -0300

    update

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -1,4 +1,6 @@
 import { ThemeToggle } from "@/components/ui/themeToggle";
+import { Home } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 export default function LoginLayout({
@@ -8,7 +10,10 @@ export default function LoginLayout({
 }>) {
   return (
     <div className="flex h-screen flex-col items-center justify-center font-sans">
-      <div className="p-4 w-full flex justify-end">
+      <div className="p-4 w-full flex justify-between items-center">
+        <Link href="/">
+          <Home />
+        </Link>
         <ThemeToggle />
       </div>
       {children}
